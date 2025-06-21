b537dc62c9b9a80dd6edac48eb9a90adbb48d59c
Sergiusz <sergiusz.sobol7@gmail.com>
2025-06-21 18:45:44 +0200

    ddded client registration endpoint /api/auth/register

---
+++ CarPartsShop/Controllers/AuthController.cs
@@ -0,0 +1,29 @@
+﻿using CarPartsShop.Data;
+using CarPartsShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarPartsShop.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] Customer customer)
+        {
+            if (TempDb.Customers.Any(u => u.Email == customer.Email))
+                return BadRequest("User with that email already exists");
+
+            customer.Id = TempDb.Customers.Count + 1;
+            TempDb.Customers.Add(customer);
+
+            return Created("", new
+            {
+                customer.Id,
+                customer.Email,
+                customer.FullName,
+                customer.Role
+            });
+        }
+    }
+}
