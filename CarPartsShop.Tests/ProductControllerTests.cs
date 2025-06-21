2310fabcba4701b3c96148174cb139a3c6b97fe7
Sergiusz <sergiusz.sobol7@gmail.com>
2025-06-21 16:50:48 +0200

    added unit and integration test

---
+++ CarPartsShop.Tests/ProductControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarPartsShop.Controllers;
+using CarPartsShop.Data;
+using CarPartsShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarPartsShop.Tests
+{
+    public class ProductControllerTests
+    {
+        [Fact]
+        public void GetAll_ReturnsAllProducts()
+        {
+            // Arrange
+            TempDb.Products =
+            [
+                new Product { Id = 1, Name = "Test A" },
+                new Product { Id = 2, Name = "Test B" }
+            ];
+
+            var controller = new ProductController();
+
+            // Act
+            var result = controller.GetAll() as OkObjectResult;
+
+            // Assert
+            Assert.NotNull(result);
+            var products = result.Value as List<Product>;
+            Assert.Equal(2, products.Count);
+        }
+    }
+}
---
+++ CarPartsShop.Tests/ProductIntegrationTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace CarPartsShop.Tests
+{
+    public class ProductIntegrationTests : IClassFixture<WebApplicationFactory<CarPartsShop.Program>>
+    {
+        private readonly HttpClient _client;
+
+        public ProductIntegrationTests(WebApplicationFactory<CarPartsShop.Program> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAllProducts_ReturnsSuccess()
+        {
+            // Act
+            var response = await _client.GetAsync("/api/product");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
